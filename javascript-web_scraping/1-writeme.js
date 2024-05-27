99f57cb60e6dfde1cc3785f072eea502581d5750
nataly <ncristina.rodriguezr@gmail.com>
2024-05-27 18:56:06 -0500

    :green_heart:point 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Obtener la ruta del archivo y la cadena de los argumentos de la línea de comandos
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+if (!filePath || !stringToWrite) {
+  console.error('Por favor, proporciona la ruta del archivo como primer argumento y la cadena a escribir como segundo argumento.');
+  process.exit(1);
+}
+
+// Escribir la cadena en el archivo
+fs.writeFile(filePath, stringToWrite, { encoding: 'utf-8' }, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('El contenido ha sido escrito exitosamente.');
+  }
+});
