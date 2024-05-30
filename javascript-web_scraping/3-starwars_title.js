da8d67d5c50b0b000a4768e10cd2b33ed063bf76
nataly <ncristina.rodriguezr@gmail.com>
2024-05-30 15:49:12 -0500

    :alien:point 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,21 +3,21 @@ const request = require('request');
 const id = process.argv[2];
 
 if (!id) {
-    console.error('Por favor, proporciona un id como primer argumento.');
-    process.exit(1);
-  }
+  console.error('Por favor, proporciona un id como primer argumento.');
+  process.exit(1);
+}
 
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${id}`;
 
 request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error al realizar la solicitud:', error);
+  if (error) {
+    console.error('Error al realizar la solicitud:', error);
+  } else {
+    const movieData = JSON.parse(body);
+    if (movieData.title) {
+      console.log(movieData.title);
     } else {
-        const movieData = JSON.parse(body);
-        if (movieData.title) {
-            console.log(movieData.title);
-        } else {
-            console.log('No se encontró una película con el ID proporcionado.');
-        }
+      console.log('No se encontró una película con el ID proporcionado.');
     }
+  }
 });
