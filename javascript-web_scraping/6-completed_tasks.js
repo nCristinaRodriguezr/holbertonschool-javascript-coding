ecf4302260b09f833ef6a481eb1289eec6e0bf17
nataly <ncristina.rodriguezr@gmail.com>
2024-05-30 18:19:17 -0500

    :monocle_face:point 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,37 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
 
-const apiUrl = process.argv[2];
+const url = 'https://jsonplaceholder.typicode.com/todos';
 
-if (!apiUrl) {
-  console.error('Usage: ./script.js <API_URL>');
-  process.exit(1);
-}
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const completedTasksByUser = {};
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error making the request:', error);
-    return;
-  }
-
-  const todos = JSON.parse(body);
-  const completedTasksByUser = {};
-
-  todos.forEach(todo => {
-    if (todo.completed) {
-      if (!completedTasksByUser[todo.userId]) {
-        completedTasksByUser[todo.userId] = 0;
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (completedTasksByUser[todo.userId]) {
+          completedTasksByUser[todo.userId] += 1;
+        } else {
+          completedTasksByUser[todo.userId] = 1;
+        }
       }
-      completedTasksByUser[todo.userId]++;
-    }
-  });
+    });
 
-  // Convert completedTasksByUser to a JSON string
-  const jsonString = JSON.stringify(completedTasksByUser, null, 2);
-
-  // Replace double quotes with single quotes for keys
-  const jsonStringWithSingleQuotes = jsonString.replace(/"(\d+)":/g, "'$1':");
-
-  // Print the final JSON string without an additional object
-  console.log(jsonStringWithSingleQuotes);
+    console.log(completedTasksByUser);
+  } else {
+    console.error('Error:', error);
+  }
 });
