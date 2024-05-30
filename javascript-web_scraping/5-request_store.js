054b3f09ffe15dc33e29aeff4c8eee2b132a94ad
nataly <ncristina.rodriguezr@gmail.com>
2024-05-30 17:33:20 -0500

    :mag:point 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Usage: ./script.js <URL> <file_path>');
+  process.exit(1);
+}
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error making the request:', error);
+    return;
+  }
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      return;
+    }
+    console.log('Content successfully saved to', filePath);
+  });
+});
