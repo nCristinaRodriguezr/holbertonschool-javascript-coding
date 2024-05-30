925507ab25da70a735a26a46b915e4a7abb4d664
nataly <ncristina.rodriguezr@gmail.com>
2024-05-30 18:07:08 -0500

    :monocle_face:point 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -29,8 +29,9 @@ request(apiUrl, (error, response, body) => {
   // Convert completedTasksByUser to a JSON string
   const jsonString = JSON.stringify(completedTasksByUser, null, 2);
 
-  // Replace double quotes with single quotes
+  // Replace double quotes with single quotes for keys
   const jsonStringWithSingleQuotes = jsonString.replace(/"(\d+)":/g, "'$1':");
 
+  // Print the final JSON string without an additional object
   console.log(jsonStringWithSingleQuotes);
 });
