462bcd8e057218d74d9a961e118b260ce2278f9c
nataly <ncristina.rodriguezr@gmail.com>
2024-05-30 17:49:52 -0500

    :mag:point 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -18,7 +18,6 @@ request(url, (error, response, body) => {
   fs.writeFile(filePath, body, 'utf8', (err) => {
     if (err) {
       console.error('Error writing to file:', err);
-      return;
     }
   });
 });
