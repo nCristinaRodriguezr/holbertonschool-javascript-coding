3d5d5bed05d09aa0616dee9fb3a9300bccf55023
nataly <ncristina.rodriguezr@gmail.com>
2024-06-08 21:14:49 -0500

    :ambulance:point 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+// Crea el servidor
+const app = http.createServer((req, res) => {
+  // Configura la respuesta HTTP con el código de estado 200 (OK) y el tipo de contenido de texto plano
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
+  // Envía "Hello Holberton School!" como respuesta
+  res.end('Hello Holberton School!\n');
+});
+
+// Escucha en el puerto 1245
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Exporta el servidor para que pueda ser utilizado en otros archivos
+module.exports = app;
