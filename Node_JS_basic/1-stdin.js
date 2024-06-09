bda8f3b6825d0d9adea04b1eaed7c999c003c474
nataly <ncristina.rodriguezr@gmail.com>
2024-06-08 19:37:13 -0500

    :lipstick:point 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+
+// Crear una interfaz readline para la entrada y salida del proceso
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Mostrar el mensaje de bienvenida
+console.log('Welcome to Holberton School, what is your name?');
+
+// Leer la entrada del usuario
+rl.on('line', (input) => {
+  // Mostrar el nombre del usuario
+  console.log(`Your name is: ${input}`);
+  
+  // Cerrar la interfaz readline
+  rl.close();
+});
+
+// Evento que se ejecuta cuando la interfaz readline se cierra
+rl.on('close', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
+});
